d2c994c5e63f5dbf0247d71b243972ff0eff59c4
Gael Chavez <gael.chavez.h@gmail.com>
2024-03-06 13:49:11 -0500

    Coding search bar & home screen features session #3

--- app/src/main/java/com/example/wjcampus/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/wjcampus/ui/home/HomeFragment.java
@@ -18,6 +18,8 @@ import com.example.wjcampus.R;
 import com.example.wjcampus.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -68,11 +70,30 @@ public class HomeFragment extends Fragment {
         Button nodeButton4 = new Button(getContext()); Button nodeButton5 = new Button(getContext());
 
         //Adding items to the saved rooms list
-        homeScreenRooms.add(new HomeScreenRoom("G03", "Floor: G", R.drawable.classroom1, nodeButton1));
-        homeScreenRooms.add(new HomeScreenRoom("124", "Floor: 2", R.drawable.classroom1, nodeButton2));
-        homeScreenRooms.add(new HomeScreenRoom("203", "Floor: 2", R.drawable.classroom1, nodeButton3));
-        homeScreenRooms.add(new HomeScreenRoom("200", "Floor: 2", R.drawable.classroom1, nodeButton4));
-        homeScreenRooms.add(new HomeScreenRoom("G12", "Floor: G", R.drawable.classroom1, nodeButton5));
+        //Ground floor
+        String currRoom = "";
+        List<String> GFloorExceptions = Arrays.asList("G06","G08", "G15", "G16", "G17", "G18", "G19", "G23", "G24", "G26", "G28", "G29", "G47", "G48", "G49", "G50", "G51"
+                ,"G52", "G53", "G54", "G55", "G56", "G57", "G58", "G59", "G60", "G61", "G62", "G63", "G66", "G67", "G72", "G73", "G80");
+        for (int a = 2; a < 87; a++) {
+            if (a < 10) {
+                currRoom = "G0" + a;
+            }
+            else {
+                currRoom = "G"+a;
+            }
+            //Dealing with exceptions before adding
+            if (!GFloorExceptions.contains(currRoom)) homeScreenRooms.add(new HomeScreenRoom(currRoom, "Floor: G", R.drawable.classroom1, nodeButton1));
+        }
+        currRoom = "";
+
+        //First and second floor
+        List<String> restOfExceptions = Arrays.asList("s", "s");
+
+//        homeScreenRooms.add(new HomeScreenRoom("G03", "Floor: G", R.drawable.classroom1, nodeButton1));
+//        homeScreenRooms.add(new HomeScreenRoom("124", "Floor: 2", R.drawable.classroom1, nodeButton2));
+//        homeScreenRooms.add(new HomeScreenRoom("203", "Floor: 2", R.drawable.classroom1, nodeButton3));
+//        homeScreenRooms.add(new HomeScreenRoom("200", "Floor: 2", R.drawable.classroom1, nodeButton4));
+//        homeScreenRooms.add(new HomeScreenRoom("G12", "Floor: G", R.drawable.classroom1, nodeButton5));
 
         //Making it visible and setting the adapter
         homeScreenRoomsView.setLayoutManager(new LinearLayoutManager(getContext()));
